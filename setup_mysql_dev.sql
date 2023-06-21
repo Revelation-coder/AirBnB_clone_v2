783b795ce563944ff162225c85500e4d4256ccc0
Revelation-coder <captainrevelation@gmail.com>
2023-06-21 01:12:25 -0700

    sql

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -4,16 +4,17 @@
 --   Grants all privileges for hbnb_dev on hbnb_dev_db.
 --   Grants SELECT privilege for hbnb_dev on performance_schema.
 
+-- Create hbnb_dev_db database if it doesn't exist
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
-CREATE USER
-    IF NOT EXISTS 'hbnb_dev'@'localhost'
-    IDENTIFIED BY 'hbnb_dev_pwd';
-GRANT ALL PRIVILEGES
-   ON `hbnb_dev_db`.*
-   TO 'hbnb_dev'@'localhost'
-   IDENTIFIED BY 'hbnb_dev_pwd';
-GRANT SELECT
-   ON `performance_schema`.*
-   TO 'hbnb_dev'@'localhost'
-   IDENTIFIED BY 'hbnb_dev_pwd';
+
+-- Create hbnb_dev user with password hbnb_dev_pwd if it doesn't exist
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+
+-- Grant all privileges on hbnb_dev_db to hbnb_dev user
+GRANT ALL PRIVILEGES ON `hbnb_dev_db`.* TO 'hbnb_dev'@'localhost';
+
+-- Grant SELECT privilege on performance_schema to hbnb_dev user
+GRANT SELECT ON `performance_schema`.* TO 'hbnb_dev'@'localhost';
+
+-- Flush privileges
 FLUSH PRIVILEGES;
