5f772308c9f5b72f4097a1554921a1b001ca60c1
Revelation-coder <captainrevelation@gmail.com>
2023-07-06 01:16:31 -0700

    pycode

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -16,16 +16,20 @@ def do_deploy(archive_path):
         archive_name_no_ext = archive_name.split('.')[0]
 
         put(archive_path, '/tmp/')
-        run('mkdir -p /data/web_static/releases/{}/'.format(archive_name_no_ext))
-        run('tar -xzf /tmp/{} -C /data/web_static/releases/{}/'.
-            format(archive_name, archive_name_no_ext))
+        run('mkdir -p /data/web_static/releases/{}/'
+            .format(archive_name_no_ext))
+        run('tar -xzf /tmp/{} -C /data/web_static/releases/{}/'
+            .format(archive_name, archive_name_no_ext))
         run('rm /tmp/{}'.format(archive_name))
-        run('mv /data/web_static/releases/{}/web_static/* /data/web_static/releases/{}/'.
-            format(archive_name_no_ext, archive_name_no_ext))
-        run('rm -rf /data/web_static/releases/{}/web_static'.format(archive_name_no_ext))
+        run('mv /data/web_static/releases/{}/web_static/* '
+            '/data/web_static/releases/{}/'
+            .format(archive_name_no_ext, archive_name_no_ext))
+        run('rm -rf /data/web_static/releases/{}/web_static'
+            .format(archive_name_no_ext))
         run('rm -rf /data/web_static/current')
-        run('ln -s /data/web_static/releases/{}/ /data/web_static/current'.
-            format(archive_name_no_ext))
+        run('ln -s /data/web_static/releases/{}/ /data/web_static/current'
+            .format(archive_name_no_ext))
         return True
-    except:
+    except Exception as e:
+        print("Error:", str(e))
         return False
