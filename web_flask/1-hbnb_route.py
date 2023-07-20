4a87069101789daf07e7a2856ed15648aaec2634
Revelation-coder <captainrevelation@gmail.com>
2023-07-20 01:17:56 -0700

    done first 3

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+1-hbnb_route.py - Start a Flask web application
+
+This script starts a Flask web application that listens on 0.0.0.0, port 5000.
+
+Routes:
+    /: display "Hello HBNB!"
+    /hbnb: display "HBNB"
+    (You must use the option strict_slashes=False in your route definition)
+"""
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    return "Hello HBNB!"
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    return "HBNB"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=False)
---
+++ web_flask/2-c_route.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+2-c_route.py - Start a Flask web application
+
+This script starts a Flask web application that listens on 0.0.0.0, port 5000.
+
+Routes:
+    /: display "Hello HBNB!"
+    /hbnb: display "HBNB"
+    /c/<text>: display "C " followed by the value of the text variable
+              (replace underscore _ symbols with a space)
+    (You must use the option strict_slashes=False in your route definition)
+"""
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    return "Hello HBNB!"
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    return "HBNB"
+
+@app.route('/c/<text>', strict_slashes=False)
+def c(text):
+    return "C {}".format(text.replace("_", " "))
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
